1ebc7ccf1a3b1a193f304b835df027b4980f64f2
casey-tran <43631898+casey-tran@users.noreply.github.com>
2019-04-15 17:15:49 -0600

    Add files via upload

--- Restaurant Postgresql Table.sql
+++ Restaurant Postgresql Table.sql
@@ -1,22 +1,24 @@
 CREATE TABLE IF NOT EXISTS restaurants (
     restaurant_id serial PRIMARY KEY,
     restaurant_name VARCHAR (50) UNIQUE NOT NULL,
-    restaurant_hours VARCHAR (75) NOT NULL,
+    restaurant_open_time VARCHAR (4) NOT NULL,
+    restaurant_close_time VARCHAR (4) NOT NULL,
+    restaurant_open_day VARCHAR (75) NOT NULL,
     restaurant_price VARCHAR (5) NOT NULL,
-    restaurant_rating FLOAT,
-    restaurant_latitude VARCHAR (15) NOT NULL,
-    restaurant_longitude VARCHAR (15) NOT NULL,
-    restaurant_alcohol VARCHAR (50)
+    restaurant_rating NUMERIC (4, 3),
+    restaurant_address VARCHAR (75) NOT NULL,
+    restaurant_alcohol VARCHAR (50),
+    restaurant_food VARCHAR (50)
 );
 
-INSERT INTO restaurants(restaurant_name, restaurant_hours, restaurant_price, restaurant_latitude, restaurant_longitude, restaurant_alcohol)
-VALUES ('Arcana Restaurant', 'Daily 4 PM - 6 PM', '$$$', '40.016202', '-105.283347', 'beer cider wine cocktails shot'),
-('The Attic Bar & Bistro', 'Mon - Sat 3 PM - 6 PM, Sun 9 PM - 2 AM', '$', '40.0162966', '-105.2828816', 'beer well draft fat albert'),
-('Bohemian Biergarten', 'Mon - Fri 3 PM - 6 PM, Mon 9 PM - 1 AM', '$$', '40.0183982', '-105.2788695', 'bier beer'),
-('Boulder Chophouse & Tavern', 'Daily 4 PM - 6 PM', '$$$', '40.0162303', '-105.2832073', 'draft beer wine well'),
-('Brasserie Ten Ten', 'Daily 3 PM - 6:30 PM', '$$', '40.0164957', '-105.2818993', 'beer house wine cocktails'),
-('Centro Mexican Kitchen', 'Mon 6 PM - 10 PM, Tue - Sun 2 PM - 6 PM, Sat - Sun 9:30 AM - 10 AM', '$$', '40.0168253', '-105.2846888', 'beer margerita wine daiquiri sangria'),
-('The Corner Bar', 'Daily 3 PM - 6 PM', '$$', '40.0193406', '-105.2794309', 'cocktails wine draft beers wells'),
-('Eureka!', 'Daily 2 PM - 6 PM, Daily 9 PM to 11 PM', '$$', '40.0171357', '-105.2822555', 'cocktails'),
-('Hapa Sushi Grill and Sake Bar', 'Mon - Sat 2:30 PM - 5:30 PM, Thu - Sat 10 PM - 12 AM', '$$', '40.0178439', '-105.280835', 'sake beer wells cocktails')
+INSERT INTO restaurants(restaurant_name, restaurant_open_time, restaurant_close_time, restaurant_open_day, restaurant_price, restaurant_address, restaurant_alcohol, restaurant_food)
+VALUES ('Arcana Restaurant', '1600', '2200', 'MON, TUES, WED, THUR, FRI, SAT, SUN', '$$$', '909 Walnut Street, Boulder, CO', 'beer cider wine cocktails shot', 'new american'),
+('The Attic Bar & Bistro', '1100', '0200', 'MON, TUES, WED, THUR, FRI, SAT, SUN', '$', '949 Walnut Street, Boulder, CO', 'beer well draft fat albert', 'american comfort'),
+('Bohemian Biergarten', '1500', '0100', 'MON, TUES, WED, THUR, FRI, SAT, SUN', '$$', '2017 13th St, Boulder, CO', 'bier beer', 'czech'),
+('Boulder Chophouse & Tavern', '1600', '2200', 'MON, TUES, WED, THUR, FRI, SAT, SUN', '$$$', '921 Walnut St, Boulder, CO', 'draft beer wine well', 'steakhouse american'),
+('Brasserie Ten Ten', '1100', '2200', 'MON, TUES, WED, THUR, FRI, SAT, SUN', '$$', '1011 Walnut St, Boulder, CO', 'beer house wine cocktails', 'french brunch'),
+('Centro Mexican Kitchen', '0730', '2200', 'MON, TUES, WED, THUR, FRI, SAT, SUN', '$$', '950 Pearl St, Boulder, CO', 'beer margerita wine daiquiri sangria', 'mexican latin'),
+('The Corner Bar', '1100', '0000', 'MON, TUES, WED, THUR, FRI, SAT, SUN', '$$', '2115 13th St, Boulder, CO', 'cocktails wine draft beers wells', 'american grill'),
+('Eureka!', '1100', '2300', 'MON, TUES, WED, THUR, FRI, SAT, SUN', '$$', '1048 Pearl St, Boulder, CO', 'cocktails', 'american'),
+('Hapa Sushi Grill and Sake Bar', '1100', '2200', 'MON, TUES, WED, THUR, FRI, SAT, SUN', '$$', '1117 Pearl St, Boulder, CO', 'sake beer wells cocktails', 'japanese asian sushi hawaiian')
 ;
