7c9495bb598858cc87adda214cf580f7518e68af
casey-tran <43631898+casey-tran@users.noreply.github.com>
2019-04-18 20:30:16 -0600

    Add files via upload

--- server.js
+++ server.js
@@ -20,19 +20,24 @@ app.use(express.static(__dirname + "/"));
 app.get('/register', function(req, res) {
 	res.render("register",{
     //local_css: "signin.css"
-});
+	});
 });
 
 app.get('/home', function(req, res){
     res.render('home',{
-    })
-})
+    });
+});
 
 
 app.get('/login', function(req, res){
     res.render('login',{
-    })
-})
+    });
+});
+
+app.get('/location', function(req, res){
+    res.render('location',{
+    });
+});
 
 // app.post('/register', function (req, res, next){
 //     const user = req.user
@@ -70,64 +75,28 @@ app.get('/filter', function(req, res) {
 });
 
 app.get("/filter/filter_result", function(req, res) {
-    //retrieve various parameters
-    var search = req.query.search_term;
     var prices = req.query.price;
     var hour = req.query.hour;
-    var food = req.query.food_type;
-    var drink = req.query.drink_type;
 
-    //initialize empty strings to hold queries
-    var search_query = "";
     var price_query = "";
     var hour_query = "";
-    var food_query = "";
-    var drink_query = "";
 
-    //if search term defined, query for any restaurant whose name, food type, or alcohol type contains search term as a substring
-    if (search != "") {
-        search_query = "(lower(restaurant_name) like lower('%" + search + "%')) or (lower(restaurant_food) like lower('%" + search + "%')) or (lower(restaurant_alcohol) like lower('%" + search + "%'))";
-    }
-
-    if (prices && typeof(prices) == "object") { //if more than one price selected, will be an object, will query by all prices connected with an 'or'
+    if (prices && typeof(prices) == "object") {
         for (i = 0; i < prices.length; i++) {
             prices[i] = "restaurant_price='" + prices[i] + "'";
         }
-        price_query = prices.join(" or ");
+        price_query = " where" + prices.join(" or ");
     }
-    else if (prices) { //if not an object, but still defined, only one price to query by
-        price_query = "restaurant_price='" + prices + "'";
+    else if (prices) {
+        price_query = " where restaurant_price='" + prices + "'";
     }
 
-    //if hour parameter defined, query by times for which the hour given is between the opening and closing time of restaurant (after the first 'or' handles cases where the restaurant closes past midnight)
     if (hour != "") {
-        hour_query = "(" + hour + " > cast (restaurant_open_time as int) and " + hour + " <= cast (restaurant_close_time as int)) or (cast (restaurant_open_time as int) >= cast (restaurant_close_time as int) and (" + hour + " >= cast (restaurant_open_time as int) or " + hour + " <= cast (restaurant_close_time as int)))";
+        hour_query = " where (" + hour + " > cast (restaurant_open_time as int) and " + hour + " <= cast (restaurant_close_time as int)) or (cast (restaurant_open_time as int) >= cast (restaurant_close_time as int) and (" + hour + " >= cast (restaurant_open_time as int) or " + hour + " <= cast (restaurant_close_time as int)))";
     }
-
-    if (food != "") {
-        food_query = "lower(restaurant_food) like lower('%" + food + "%')";
-    }
-
-    if (drink != "") {
-        drink_query = "lower(restaurant_alcohol) like lower('%" + drink + "%')";
-    }
-
-    //construct the final query where clause
-    queries_unchecked = [search_query, price_query, hour_query, food_query, drink_query];
-    queries = [];
-    queries_unchecked.forEach(function(query) {
-        if (query != "") {
-            queries.push(query);
-        }
-    })
-    queries = queries.join(") and (");
-    if (queries != "") {
-        queries = " where (" + queries + ")";
-    }
-
-    //construct final query
+    
     var starter_query = "select * from restaurants";
-    var filter_query = starter_query + queries;
+    var filter_query = starter_query + price_query + hour_query;
     filter_query += " order by restaurant_name;";
 
     console.log(filter_query);
